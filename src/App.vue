71a694b74f8c4c510699deb2bd621cc527cd8c1e
nutanek <nutcs@hotmail.com>
2017-10-23 13:50:00 +0700

    create mockup

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,39 @@
 <template>
-  <div id="app">
-    <button class="btn btn-success">OK</button>
-  </div>
+    <div id="app" class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h1>DIY You App</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <app-mockup></app-mockup>
+            </div>
+            <div class="col-md-7">
+            
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// import { bBtn } from 'bootstrap-vue/lib/components'
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import Mockup from './components/Mockup.vue'
+    export default {
+        name: 'app',
+        components: {
+            'app-mockup': Mockup
+        },
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+    h1 {
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
 </style>
---
+++ src/components/Mockup.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="wrapper -small">
+        <div class="row top justify-content-center">
+            <div>
+                <div class="io -camera"></div>
+                <div class="io -speaker"></div>
+            </div>
+        </div>
+        <div class="col screen">
+            
+        </div>
+        <div class="row bottom justify-content-center">
+            <div class="home-button"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .wrapper {
+        width: 350px;
+        padding-left: 10px;
+        padding-right: 10px;
+        border: 2px solid #AAAAAA;
+        border-radius: 30px;
+        &.-small {
+            transform: scale(0.9);
+        }
+        > .top {
+            margin-top: 30px;
+            margin-bottom: 30px;
+            .io {
+                height: 12px;
+                border: 2px solid #AAAAAA;
+                float: left;
+                &.-camera {
+                    margin-right: 5px;
+                    width: 12px;
+                    border-radius: 100%;
+                }
+                &.-speaker {
+                    width: 100px;
+                    border-radius: 10px;
+                }
+            }
+        }
+        > .bottom {
+            .home-button {
+                margin-top: 20px;
+                margin-bottom: 20px;
+                width: 40px;
+                height: 40px;
+                border: 2px solid #AAAAAA;
+                border-radius: 100%;
+            }
+        }
+        > .screen {
+            height: 540px;
+            border: 2px solid #AAAAAA;
+        }
+    }
+</style>
